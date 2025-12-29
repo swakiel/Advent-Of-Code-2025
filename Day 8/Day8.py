a8b501e0fd8d5b3a812b306621d17b36d684b97a
Sam <sam.wakiel@gmail.com>
2025-12-29 02:18:37 +0000

    Day 8 Part 2

--- Day 8/Day8.py
+++ Day 8/Day8.py
@@ -2,7 +2,6 @@
 
 from math import sqrt
 import heapq
-from collections import Counter
 
 def distance(coord1, coord2):
     x1, y1, z1 = coord1
@@ -20,12 +19,9 @@ for i in range(len(matrix) - 1):
 
 circuits = []
 
-counter = Counter()
 count = 0
 while count < 1000:
     dist, i, j = heapq.heappop(heap)
-    counter[i] += 1
-    counter[j] += 1
     added = False
     new_circuits = []
     new_circuit = set()
@@ -39,6 +35,7 @@ while count < 1000:
             if len(new_circuit) == 0:
                 new_circuit = circuit
             else:
+                # merging sets together which share a connection
                 new_circuit = new_circuit | circuit
     if not added:
         new_circuit = {i, j}
@@ -51,4 +48,39 @@ circuits.sort(key=lambda x: len(x), reverse=True)
 
 print(len(circuits[0]) * len(circuits[1]) * len(circuits[2]))
 
+# PART 2
+
+heap = []
+for i in range(len(matrix) - 1):
+    for j in range(i+1, len(matrix)):
+        coord1, coord2 = matrix[i], matrix[j]
+        heapq.heappush(heap, (distance(coord1, coord2), i, j))
+
+circuits = []
+
+while True:
+    dist, i, j = heapq.heappop(heap)
+    added = False
+    new_circuits = []
+    new_circuit = set()
+    temp_circuits = circuits.copy()
+    for circuit in circuits:
+        if i in circuit or j in circuit:
+            temp_circuits.remove(circuit)
+            circuit.add(i)
+            circuit.add(j)
+            added = True
+            if len(new_circuit) == 0:
+                new_circuit = circuit
+            else:
+                # merging sets together which share a connection
+                new_circuit = new_circuit | circuit
+    if not added:
+        new_circuit = {i, j}
+    new_circuits.append(new_circuit)
+    temp_circuits += new_circuits
+    circuits = temp_circuits
+    if len(circuits[0]) == 1000:
+        print(matrix[i][0] * matrix[j][0])
+        break
 
